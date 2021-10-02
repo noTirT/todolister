95c17f303e48def25e6549b65f5727734deefb15
Tom Manger <tom.manger@vaudience.net>
2021-10-02 21:54:53 +0200

    Initial commit

---
+++ src/main/java/com/tom/todolister/TodolisterApplication.java
@@ -0,0 +1,13 @@
+package com.tom.todolister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/todolister/TodolisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.todolister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
