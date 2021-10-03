843caf31ae7bfa1a25aeef023d40ee6bc0d7d54f
Tom Manger <tom.manger@vaudience.net>
2021-10-03 12:32:16 +0200

    Added getByTitle API endpoint to controller

--- src/main/java/com/tom/todolister/controller/TodoController.java
+++ src/main/java/com/tom/todolister/controller/TodoController.java
@@ -41,10 +41,23 @@ public class TodoController {
             @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
             @ApiResponse(responseCode = "404", description = "No todo-items with the given id found")
     })
-    @GetMapping("/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<TodoItem> getTodoItemById(
             @Parameter(description = "Id to be searched for") @PathVariable String id) {
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get todo-items by title", description = "Searches database for a todo-item with a given title and returns it")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todo-item(s) found",
+                    content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TodoItem.class)))}),
+            @ApiResponse(responseCode = "400", description = "Invalid title supplied"),
+            @ApiResponse(responseCode = "404", description = "No todo-items with the given title found")
+    })
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<TodoItem>> getTodoItemsByTitle(
+            @Parameter(description = "Title to be searched for", example = "Clean desk") @PathVariable String title) {
+        return new ResponseEntity<>(service.findByTitle(title), HttpStatus.OK);
     }
 
     @Operation(summary = "Save a new todo-item to the database", description = "Saves a new todo-item to the database")
